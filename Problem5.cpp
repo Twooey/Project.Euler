11fe701968ffd9df6457cc717411d20e4293623a
kb3pev <kb3pev@gmail.com>
2014-04-10 13:05:37 -0400

    Update Problem5.cpp
    Removing Redundant loops.

--- Problem5.cpp
+++ Problem5.cpp
@@ -13,6 +13,8 @@ I'm not sure if it would be faster to check only the non prime numbers.
 #include <iostream>
 #include <vector>
 
+// isPrime is the Sieve of Eratosthenes
+
 void isPrime(std::vector<bool> &primes){
 
     primes[1] = false;
@@ -30,32 +32,29 @@ int main(){
 
     int a = 0;
     long long check = 0;
-    long long primeLCM = 1;
+    long long primeLCM = 1; //needs to be set to 1. 
     std::vector<bool>primes;
-    std::vector<int>primeFactors;
-
 
     std::cout << "What is the smallest number this is evenly divisible by 1 and (up to 179): ";
     std::cin >> a;
-    primes.resize(a,true);
-    isPrime(primes);
+    primes.resize(a,true); //resizing the primes array to the proper size.
 
+    
+    isPrime(primes); //calculating primes
+    
+    //Putting all primes in a vector. This buy's some effeciency by 
+    //not checking if each number is a prime.
     for(int b = 0; b < primes.size(); b++)
     {
         if(primes[b] == true)
-            primeFactors.push_back(b);
+            std::cout << primes[b] << std::endl;
+            primeLCM *= primes[b];
     }
-
-    for (int c = 0; c < primeFactors.size(); c++){
-        std::cout << primeFactors[c] << std::endl;
-    }
-
-    for(int b = 0; b < primeFactors.size(); b++){
-        primeLCM *= primeFactors[b];
-    }
+    
     std::cout << primeLCM << std::endl;
-    check = primeLCM;
-
+    check = primeLCM; 
+    
+    //Check's if multples of the primeLCM are 
     for(int c = 1; c  <= a; c++){
         if(check % c != 0){
             c = 1;
