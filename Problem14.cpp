9869bd7793d00b4675e16062b1521f993d3f55d8
kb3pev <kb3pev@gmail.com>
2014-04-12 16:19:18 -0400

    Project 14
    Now Working

--- Problem14.cpp
+++ Problem14.cpp
@@ -1,32 +1,35 @@
 /*Project Euler 14.
-WIP, still broken.
+The following iterative sequence is defined for the set of positive integers:
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+Which starting number, under one million, produces the longest chain?
+NOTE: Once the chain starts the terms are allowed to go above one million
 */
 
 #include <iostream>
 #include <vector>
 
-
-class Collatz{
-public:
-        int num = 0;
-        std::vector<int>collatz;
-};
-
-
 int main (){
 
     const int maxnum = 1000000;
-    std::vector<Collatz>numbers(maxnum);
-
-    for(int a = 2; a < maxnum; a++){
-        numbers[a].num = a;
-
-        for(int n = a; n != 1;){
+    std::vector<int>counter(maxnum,0);
+    int largest = 0;
+    int a1 = 0;
+    for(int a = 1 ; a <= maxnum ; a++){
+        for(unsigned long long n = a; n != 1; ++counter[a]){
             if (n % 2 == 0) {n = n/2;}
             else {n = 3*n+1;}
-            numbers[a].collatz.push_back(n);
+        }
+        if(largest < counter[a]){
+                largest = counter[a];
+                a1 = a;
         }
     }
+
+    std::cout << a1;
     return 0;
 }
 
